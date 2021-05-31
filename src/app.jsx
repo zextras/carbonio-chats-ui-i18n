a9820de11f95df4c037f64aea27933d7309c766f
Lisa Parma <lisa.parma@zextras.com>
2021-05-31 12:18:21 +0200

    Init zapp-team repo

---
+++ src/app.jsx
@@ -0,0 +1,50 @@
+/*
+ * *** BEGIN LICENSE BLOCK *****
+ * Copyright (C) 2011-2019 ZeXtras
+ *
+ * The contents of this file are subject to the ZeXtras EULA;
+ * you may not use this file except in compliance with the EULA.
+ * You may obtain a copy of the EULA at
+ * http://www.zextras.com/zextras-eula.html
+ * *** END LICENSE BLOCK *****
+ */
+
+import React, { useEffect } from 'react';
+import {
+	setCreateOptions, setMainMenuItems, setRoutes, store
+} from '@zextras/zapp-shell';
+import { combineReducers } from '@reduxjs/toolkit';
+
+export default function App() {
+	console.log('Hello from zapp-team');
+	/* Here an example on how to use this entry point.
+	 * Have fun :)
+	 * useEffect(() => {
+	 * 	store.setReducer(
+	 * 		combineReducers({}),
+	 * 	);
+	 * 	setRoutes([]);
+	 * 	setCreateOptions([{}]);
+	 * }, []);
+	 */
+	useEffect(() => {
+		setRoutes([
+			{
+				route: '/',
+				view: () => <div>Team</div>
+			}
+		]);
+	}, []);
+
+	setMainMenuItems([
+		{
+	  id: 'team-root',
+	  icon: 'TeamOutline',
+	  to: '/',
+	  label: 'Team Home',
+	  items: []
+		}
+	]);
+
+	return null;
+}
